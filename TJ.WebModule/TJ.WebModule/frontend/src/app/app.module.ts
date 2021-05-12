60901433d7c213322f34295e9f63d1086ed52623
Fares Steen <Fares.steen@gmail.com>
2021-05-12 18:55:22 +0200

    add ng components

--- TJ.WebModule/TJ.WebModule/frontend/src/app/app.module.ts
+++ TJ.WebModule/TJ.WebModule/frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CreateDeviceComponent } from './create-device/create-device.component';
+import { DevicesTableComponent } from './devices-table/devices-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CreateDeviceComponent,
+    DevicesTableComponent
   ],
   imports: [
     BrowserModule,
